98f2f2134a2c6c6076270f23836e4fbc0f18bd7b
hariseldon99 <hariseldon99@gmail.com>
2015-09-11 12:20:20 +0200

    m

---
+++ __init__.py
@@ -0,0 +1,22 @@
+"""
+    Discrete Truncated Wigner Approximation for quantum spins
+    and transverse fields with time-periodic drive
+
+    * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    * Copyright (c) 2015 Analabha Roy (daneel@utexas.edu)
+    *
+    *This is free software: you can redistribute it and/or modify it under
+    *the terms of version 2 of the GNU Lesser General Public License
+    *as published by the Free Software Foundation.
+    *Notes:
+    *1. The initial state is currently hard coded to be the classical ground
+    *    state
+    *2. Primary references are
+    *   Anatoli: Ann. Phys 325 (2010) 1790-1852
+    *   Mauritz: arXiv:1209.3697
+    *   Schachenmayer: arXiv:1408.4441
+    * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+"""
+from __future__ import division, print_function
+__all__ = ["dtwa_quantum_spins", "reductions","spin_squeezing"]
+from dtwa_quantum_spins import *
