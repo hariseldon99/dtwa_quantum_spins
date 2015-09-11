684fd71c643307b87f7a95eb6f78959bcf068627
hariseldon99 <hariseldon99@gmail.com>
2015-09-11 14:08:52 +0200

    m

--- dtwa_2d_spins.py
+++ dtwa_2d_spins.py
@@ -12,11 +12,24 @@ def run_dtwa():
 
   #Parameters
   lattice_shapes = [(4,2)]
+  alpha = 1.0
   jx, jy, jz = 0.0, 0.0, -1.0
   hx, hy, hz = -0.2, 0.0, 0.0
   niter = 2000
  
   for l in lattice_shapes:
+    
+    #Build the hopping matrix
+    jmat = np.zeros(l)
+    r, c = l
+    latsize = r * c
+    for mu in xrange(latsize):
+      for nu in xrange(latsize):
+	mux, nux = np.floor(nu/c), np.floor(mu/c)
+	muy, nuy = nu%r, mu%r
+	dmn = np.sqrt((mux-nux)**2+(muy-nuy)**2)
+	jmat[mu,nu] = 1.0/np.pow(dmn,alpha)
+	
     #Initiate the parameters in object
     p = dtwa.ParamData(hopmat=jmat,norm=1.0, latshape=lattice_shape,\
 			      jx=jx, jy=jy, jz=jz, hx=hx, hy=hy, hz=hz)
@@ -35,7 +48,7 @@ def run_dtwa():
     
     #Initiate the DTWA system with the parameters and niter
     d = dtwa.Dtwa_System(p, comm, n_t=niter, file_output=True, \
-		      s_order=True, verbose=True, sitedata=False)
+		      s_order=False, verbose=True, sitedata=False)
 
     #Prepare the times
     t0 = 0.0
