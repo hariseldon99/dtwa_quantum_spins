c7e3f3bb8d4bad35b84f78e26e6a30d049bc2645
hariseldon99 <hariseldon99@gmail.com>
2015-09-29 19:21:25 +0200

    m

--- scripts/bollinger.py
+++ scripts/bollinger.py
@@ -27,23 +27,23 @@ def run_dtwa():
   
   (row, col) = jmat.shape  
 
+  offset = os.environ['PBS_ARRAY_INDEX']
   #Initiate the parameters in object
   p = dtwa.ParamData(hopmat=(jmat+jmat.T),norm=1.0, latsize=row,\
 		      jx=jx, jy=jy, jz=jz, hx=hx, hy=hy, hz=hz)
 
-  p.output_magx = "sx_time_beta_" + filename + "_2ndorder.txt"
-  p.output_magy = "sy_time_beta_" + filename + "_2ndorder.txt"
-  p.output_magz = "sz_time_beta_" + filename + "_2ndorder.txt"
+  p.output_magx = str(offset) + "sx_time_beta_" + filename + ".txt"
+  p.output_magy = str(offset) + "sy_time_beta_" + filename + ".txt"
+  p.output_magz = str(offset) + "sz_time_beta_" + filename + ".txt"
   
-  p.output_sxvar = "sxvar_time_beta_"+ filename  + "_2ndorder.txt"
-  p.output_syvar = "syvar_time_beta_"+ filename  + "_2ndorder.txt"
-  p.output_szvar = "szvar_time_beta_"+ filename  + "_2ndorder.txt"
+  p.output_sxvar = str(offset) + "sxvar_time_beta_"+ filename  + ".txt"
+  p.output_syvar = str(offset) + "syvar_time_beta_"+ filename  + ".txt"
+  p.output_szvar = str(offset) + "szvar_time_beta_"+ filename  + ".txt"
 
-  p.output_sxyvar = "sxyvar_time_beta_"+ filename + "_2ndorder.txt"
-  p.output_sxzvar = "sxzvar_time_beta_"+ filename + "_2ndorder.txt"
-  p.output_syzvar = "syzvar_time_beta_"+ filename + "_2ndorder.txt"
+  p.output_sxyvar = str(offset) + "sxyvar_time_beta_"+ filename + ".txt"
+  p.output_sxzvar = str(offset) + "sxzvar_time_beta_"+ filename + ".txt"
+  p.output_syzvar = str(offset) + "syzvar_time_beta_"+ filename + ".txt"
   
-  offset = os.environ['PBS_ARRAY_INDEX']
   
   #Initiate the DTWA system with the parameters and niter
   d = dtwa.Dtwa_System(p, comm, n_t=niter, file_output=True, seed_offset=offset,\
--- scripts/runscript_rhasatsha_jobarrays.sh
+++ scripts/runscript_rhasatsha_jobarrays.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #########################################################################
 ## Name of my job
-#PBS -N dtwa
+#PBS -N bolliger
 #PBS -l walltime=7:00:00:00
 #########################################################################
 ##Export all PBS environment variables
@@ -13,34 +13,21 @@
 ##Number of nodes and procs per node.
 ##The ib at the end means infiniband. Use that or else MPI gets confused 
 ##with ethernet
-#PBS -l nodes=1:ppn=8:scratch
+#PBS -l nodes=20:ppn=8:scratch
 #########################################################################
 ##Send me email when my job aborts, begins, or ends
 #PBS -m ea
 #PBS -M daneel@sun.ac.za
 #########################################################################
 #Job array 
-#PBS -J 1-50
+#PBS -t 1-50
 
 SCRIPT="./run_dtwa.py"
 
 # make sure I'm the only one that can read my output
 umask 0077
-TMP=/scratch2/$PBS_JOBID
-mkdir -p $TMP
-
-if [ ! -d "$TMP" ]; then
-	echo "Cannot create temporary directory. Disk probably full."
-	exit 1
-fi
 
 cd $PBS_O_WORKDIR
-
-# copy the input files to $TMP
-echo "Copying from ${PBS_O_WORKDIR}/ to ${TMP}/"
-/usr/bin/rsync -vax "${PBS_O_WORKDIR}"/ ${TMP}/
-
-cd $TMP
 
 #########################################################################
 ##Make a list of allocated nodes(cores)
@@ -58,11 +45,5 @@ BEGINTIME=$(date +"%s")
 mpirun -np $NO_OF_CORES python $SCRIPT 
 ENDTIME=$(date +"%s")
 ELAPSED_TIME=$(($ENDTIME-$BEGINTIME))
-
-# job done, copy everything back 
-echo "Copying from ${TMP}/ to ${PBS_O_WORKDIR}/"
-/usr/bin/rsync -vax ${TMP}/ "${PBS_O_WORKDIR}/"
-# delete my temporary files
-[ $? -eq 0 ] && /bin/rm -rf ${TMP}
 
 echo "#Runtime: $(($ELAPSED_TIME / 60)) minutes and $(($ELAPSED_TIME % 60)) seconds."
