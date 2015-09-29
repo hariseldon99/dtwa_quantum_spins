a5cb8002633420af6796435e6b0a474b4cf3213e
hariseldon99 <hariseldon99@gmail.com>
2015-09-29 18:38:16 +0200

    m

--- scripts/bollinger.py
+++ scripts/bollinger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 import sys
+import os
 from mpi4py import MPI
 sys.path.append("/home/daneel/dtwa_quantum_spins/")
 import dtwa_quantum_spins as dtwa
@@ -13,7 +14,7 @@ def run_dtwa():
   #Parameters
   jx, jy, jz = 0.0, 0.0, 1.0
   hx, hy, hz = 1.0, 0.0, 0.0
-  niter = 100000
+  niter = 2000
   
   #Import the hopping matrix
   filename = "mode_code_1443141813_Jij.csv"
@@ -35,15 +36,17 @@ def run_dtwa():
   p.output_sxyvar = "sxyvar_time_beta_"+ filename + "_2ndorder.txt"
   p.output_sxzvar = "sxzvar_time_beta_"+ filename + "_2ndorder.txt"
   p.output_syzvar = "syzvar_time_beta_"+ filename + "_2ndorder.txt"
-    
+  
+  offset = os.environ['PBS_ARRAY_INDEX']
+  
   #Initiate the DTWA system with the parameters and niter
-  d = dtwa.Dtwa_System(p, comm, n_t=niter, file_output=True, \
-      s_order=True, verbose=True, sitedata=False)
+  d = dtwa.Dtwa_System(p, comm, n_t=niter, file_output=True, seed_offset=offset,\
+      s_order=True, verbose=False, sitedata=False)
 
   #Prepare the times
   t0 = 0.0
   ncyc = 1.0
-  nsteps = 200
+  nsteps = 100
 
   data = d.evolve((t0, ncyc, nsteps), sampling="all")
 
