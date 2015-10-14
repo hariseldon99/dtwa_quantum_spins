44fa8ed7088b1dba7e73d62f7233303c712c358d
hariseldon99 <hariseldon99@gmail.com>
2015-10-14 15:01:16 +0200

    m

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+# Author:  Analabha roy
+# Contact: daneel@utexas.edu
+
 """
     Discrete Truncated Wigner Approximation (dTWA) for quantum spins
     and transverse fields with time-periodic drive
@@ -12,11 +15,16 @@
     *1. The initial state is currently hard coded to be the classical ground
     *    state
     *2. Primary references are
+    *   PRM:  arXiv:1510.03768
     *   Anatoli: Ann. Phys 325 (2010) 1790-1852
     *   Mauritz: arXiv:1209.3697
     *   Schachenmayer: arXiv:1408.4441
     * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 """
+__version__   = '0.1'
+__author__    = 'Analabha Roy'
+__credits__   = 'Lorenzo Pucci, NiTheP Stellenbosch'
+
 from __future__ import division, print_function
 __all__ = ["dtwa_quantum_spins", "reductions","redirect_stdout"]
 from dtwa_quantum_spins import *
